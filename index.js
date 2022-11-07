254dcce2b16af00f6cc1cf09cb601d41d243aa7e
Aleksi Virkkala <aleksi.virkkala@gmail.com>
2022-11-07 10:22:33 +0200

    Copy video link as markdown link

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 (() => {
-  const videoElement = document.querySelector("video#secondaryVideo");
-  const currentTime = videoElement.currentTime;
-  const parsedURL = getUrlWithoutStartParameter(document.URL);
-  const timestampUrl = `${parsedURL}&start=${currentTime}`;
-  console.log(timestampUrl);
-  navigator.clipboard.writeText(timestampUrl);
+  const title = document.title;
+  const timestampUrl = createTimestampURL();
+  const mdLink = createMarkdownLink(title, timestampUrl);
+  console.log(mdLink);
+  navigator.clipboard.writeText(mdLink);
 })();
 
 function getUrlWithoutStartParameter(url) {
@@ -14,4 +13,18 @@ function getUrlWithoutStartParameter(url) {
   }
   const parsedURL = url.substr(0, startPos);
   return parsedURL;
+}
+
+function createTimestampURL() {
+  const videoElement =
+    document.querySelector("video#secondaryVideo") || // Dual player
+    document.querySelector("video#primaryVideo"); // Single player
+  const currentTime = videoElement.currentTime;
+  const parsedURL = getUrlWithoutStartParameter(document.URL);
+  const timestampUrl = `${parsedURL}&start=${currentTime}`;
+  return timestampUrl;
+}
+
+function createMarkdownLink(title, url) {
+  return `[${title}](${url})`;
 }
